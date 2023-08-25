569805aa1e1608c9ac8525e439949425b95e7ff6
Tiện Thanh <tienmt@imt-soft.com>
2023-08-25 16:29:39 +0700

    Update

--- outing-schedule-project/src/components/TimesTable.vue
+++ outing-schedule-project/src/components/TimesTable.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { computed, onMounted, ref } from "vue";
 import { AppStore } from "@/stores";
+import { useConfirm } from "primevue/useconfirm";
+
+const confirm = useConfirm();
 
 const APP_STORE = AppStore();
 
@@ -27,6 +30,21 @@ const schedule = ref([
   },
 ]);
 
+const onClickIconDelete = () => {
+  confirm.require({
+    message: "Bạn có muốn xóa chuyến đi này?",
+    header: "Xác nhận xóa",
+    icon: "pi pi-info-circle",
+    acceptLabel: "Đồng ý",
+    acceptClass: "p-button-danger",
+    rejectLabel: "Hủy bỏ",
+    rejectClass: "p-button-outlined",
+    accept: () => {
+      //
+    },
+  });
+};
+
 onMounted(async () => {
   await APP_STORE.onActionGetListTrip();
   console.log(listTrip.value);
@@ -34,9 +52,9 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="w-full card bg-black-alpha-50">
+  <div style="background-color: #c7dae8" class="w-full card">
     <ScrollPanel style="width: 100%; height: 20rem">
-      <div style="width: 1500px" class="flex bg-black-alpha-30">
+      <div style="width: 1500px" class="flex">
         <div
           v-for="(h, index) in times"
           :key="index"
@@ -57,12 +75,73 @@ onMounted(async () => {
           </div>
           <div
             :style="{ width: `${item?.coordinates?.widthActive}` }"
-            class="h-5rem bg-white border-left-1"
+            class="item bg-white border-left-1 border-right-1 p-2 relative cursor-pointer"
           >
-            {{ item?.coordinates?.widthActive }}
+            <div class="flex gap-2">
+              <img
+                :src="item?.image"
+                class="w-5rem h-5rem box-shadow-1 border-circle"
+                alt=""
+              />
+              <div class="flex flex-column gap-2">
+                <span class="font-bold flex align-items-center gap-1">
+                  <i style="font-size: 0.8rem" class="pi pi-map-marker" />
+                  {{ item?.place_name }}
+                </span>
+
+                <span
+                  class="fixed-text text-800 line-height-2 flex align-items-center gap-1"
+                >
+                  <i style="font-size: 0.8rem" class="pi pi-home" />
+                  {{ item?.address }}
+                </span>
+
+                <div class="flex flex-column gap-1">
+                  <span
+                    class="text-custom-mini text-800 flex align-items-center gap-1"
+                  >
+                    <i style="font-size: 0.7rem" class="pi pi-flag" />
+                    {{ item?.start_time }}
+                  </span>
+
+                  <span
+                    class="text-custom-mini text-800 flex align-items-center gap-1"
+                  >
+                    <i style="font-size: 0.7rem" class="pi pi-stop-circle" />
+                    {{ item?.end_time }}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <!-- Eidt -->
+            <div
+              class="option absolute flex align-items-center pl-3 gap-3 h-full top-0 left-0 right-0 bottom-0 bg-black-alpha-60"
+            >
+              <i
+                class="pi pi-pencil on-click-3 w-3rem h-3rem flex justify-content-center align-items-center border-1 border-circle text-white"
+              />
+
+              <i
+                @click="onClickIconDelete"
+                class="pi pi-trash on-click-3 w-3rem h-3rem flex justify-content-center align-items-center border-1 border-circle text-white"
+              />
+            </div>
           </div>
         </div>
       </div>
     </ScrollPanel>
   </div>
+
+  <ConfirmDialog />
 </template>
+
+<style scoped>
+.option {
+  opacity: 0;
+  transition: all 0.4s ease;
+}
+
+.item:hover .option {
+  opacity: 1;
+}
+</style>
--- outing-schedule-project/src/main.js
+++ outing-schedule-project/src/main.js
@@ -3,6 +3,9 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import ConfirmationService from "primevue/confirmationservice";
+import ToastService from "primevue/toastservice";
+
 // PrimeVue Component
 import "primevue/resources/themes/lara-light-indigo/theme.css";
 import "primevue/resources/primevue.min.css";
@@ -19,13 +22,19 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 
 import ScrollPanel from "primevue/scrollpanel";
+import ConfirmDialog from "primevue/confirmdialog";
+import Toast from "primevue/toast";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue);
+app.use(ConfirmationService);
+app.use(ToastService);
 
 app.component("ScrollPanel", ScrollPanel);
+app.component("ConfirmDialog", ConfirmDialog);
+app.component("Toast", Toast);
 
 app.mount("#app");
