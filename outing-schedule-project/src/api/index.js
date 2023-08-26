be3557e4751058357048dc9fce396ec424b41a87
Tiện Thanh <tienmt@imt-soft.com>
2023-08-26 16:14:34 +0700

    update api

--- outing-schedule-project/src/api/index.js
+++ outing-schedule-project/src/api/index.js
@@ -16,11 +16,13 @@ class ApiApp {
 export const API_APP = new ApiApp();
 
 const AxiosInstance = axios.create({
+  baseURL: "https://be-scheduling.onrender.com/api/v1/",
+
   // baseURL: "http://localhost:3000/api/v1/",
 
   // baseURL: "http://192.168.88.1.4:3000/api/v1/",
 
-  baseURL: "http://192.168.88.147:3000/api/v1/",
+  // baseURL: "http://192.168.88.147:3000/api/v1/",
 });
 
 export { AxiosInstance };
